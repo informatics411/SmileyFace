84cd1ab82b0758d42fc88e19d366239995b5febf
Eleven Fifty Coding <1150Coding@jinns-Mac-mini.local>
2023-05-19 10:31:10 -0400

    Update References, all; add MCEFC.Design

---
+++ SmileyFace.Data/Entities/Emoji.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SmileyFacePOC.Data.Entities
+{
+	public class Emoji
+	{
+		public Emoji()
+		{
+		}
+
+		public int Id { get; set; }
+		public string EmojiUnicode { get; set; }
+		public string EmojiMeaning { get; set; }
+		public string EmojiGenre { get; set; }
+		public int EmojiLocation { get; set; }
+		public int NextEmoji { get; set; }
+
+		public int EmojiRank { get; set; }
+
+
+    }
+}
+
