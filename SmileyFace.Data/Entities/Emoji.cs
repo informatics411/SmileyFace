dabbc3b6dd095535cae86187e8bd17a6b11bb006
Eleven Fifty Coding <1150Coding@jinns-Mac-mini.local>
2023-05-22 08:35:17 -0400

    Tables +  Monday v1s

--- SmileyFace.Data/Entities/Emoji.cs
+++ SmileyFace.Data/Entities/Emoji.cs
@@ -15,21 +15,26 @@ namespace SmileyFace.Data.Entities
         public int EmojiRank { get; set; }
 
         public string Unicode { get; set; }
+        public string Itself { get; set; }
         public string Genre { get; set; }
-
-		public int Location { get; set; }
-		public bool IsNested { get; set; }  //in the nest...true signifies hasGuardian; false signifies Unnested, untethered idea on ideaMap
-		public int Direction { get; set; } //45• mapping up, right; 90• right,right; 135• right, down; 180 • down, down; 225• down, left; 270• left, left; 315• left, up; 360• up, up
-		
-        [ForeignKey("IdeaMap")]
-        public int IsGuardian { get; set; } //is nest guardian
-
+        public string AltCategory1 { get; set; }
         public string Meaning { get; set; }
-        public string Alt1Meaning { get; set; }
-        public string Alt2Meaning { get; set; }
-        public string Alt3Meaning { get; set; }
-        public string Alt4Meaning { get; set; }
-        public string Alt5Meaning { get; set; }
+        public string? Alt1Meaning { get; set; }
+        public string? Alt2Meaning { get; set; }
+        public string? Alt3Meaning { get; set; }
+
+        //public string? Alt4Meaning { get; set; }
+        //public string? Alt5Meaning { get; set; }
+
+        // public int Location { get; set; }
+        // public bool IsNested { get; set; }  //in the nest...true signifies hasGuardian; false signifies Unnested, untethered idea on ideaMap
+        // public int Direction { get; set; } //45• mapping up, right; 90• right,right; 135• right, down; 180 • down, down; 225• down, left; 270• left, left; 315• left, up; 360• up, up
+
+        //[ForeignKey("IdeaMap")]
+        //public int IsGuardian { get; set; } //is nest guardian
+
+        
+        
     }
 }
 
