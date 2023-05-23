917574b3dceb1685d3343bf39ecf10925e877808
Eleven Fifty Coding <1150Coding@jinns-Mac-mini.local>
2023-05-23 11:11:29 -0400

    54

--- SmileyFace.Data/Entities/Emoji.cs
+++ SmileyFace.Data/Entities/Emoji.cs
@@ -15,8 +15,8 @@ namespace SmileyFace.Data.Entities
         public string Unicode { get; set; }
         public string Itself { get; set; }
         public string Genre { get; set; }
-        public string AltCategory1 { get; set; }
-        public string? Meaning { get; set; }
+        public string? AltCategory1 { get; set; }
+        public string Meaning { get; set; }
         public string? Alt1Meaning { get; set; }
         public string? Alt2Meaning { get; set; }
         public string? Alt3Meaning { get; set; }
